e7c0e6b637baa7458f691d615eb06e3c6856e04c
Walmir Pereira De Lima <79805351+walliots@users.noreply.github.com>
2026-05-21 19:39:51 -0300

    ci: adiciona Jenkinsfile

---
+++ Jenkinsfile.bat
@@ -0,0 +1,32 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('Build') {
+            steps {
+                bat 'mvn clean package -DskipTests'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                bat 'mvn test'
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Build realizado com sucesso!'
+        }
+        failure {
+            echo 'Build falhou!'
+        }
+    }
+}
